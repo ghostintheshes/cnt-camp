a77ca5e867f5a88481656b9c38295a9096906789
qiutianyang <dezbale@hotmail.com>
2022-07-27 17:06:08 +0800

    refactor: fix q2 and q3, log information explicitly
    set env at the beginning; use ClientIP in gin or getRealIP

--- module2/app/api.go
+++ module2/app/api.go
@@ -1,19 +1,27 @@
 package app
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func Q1(ctx *gin.Context) {
 	Header := ctx.Request.Header
 	for k, v := range Header {
-		ctx.Header(k, v[0])
+		for _, value := range v {
+			ctx.Writer.Header().Add(k, value)
+		}
 	}
-	ctx.JSON(200, Header)
+	ctx.JSON(200, fmt.Sprintf("Headers has been written:\n%s", Header))
 }
 
 func Q2(ctx *gin.Context) {
+	os.Setenv("VERSION", "v1.0")
 	ver := os.Getenv("VERSION")
 	if ver != "" {
 		ctx.Header("VERSION", ver)
@@ -22,8 +30,30 @@ func Q2(ctx *gin.Context) {
 }
 
 func Q3(ctx *gin.Context) {
-	//gin提供log中间件，默认输出
-	ctx.JSON(200, "logging done")
+	Ip := ctx.ClientIP()
+	//Ip := getRealIP(ctx.Request)
+	log.Printf("Client IP is %s,  Return Code: %d", Ip, 200)
+	ctx.Status(200)
+}
+
+func getRealIP(request *http.Request) string {
+	ip := ""
+	if h := request.Header.Get("X-Forwarded-For"); h != nil {
+		ip = strings.TrimSpace(strings.Split(h, ",")[0])
+		if ip != nil {
+			return ip
+		}
+	}
+	if h := request.Header.Get("X-Real-Ip"); h != nil {
+		ip := strings.TrimSpace(strings.Split(h, ",")[0])
+		if ip != nil {
+			return ip
+		}
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(request.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ip
 }
 
 func Healthz(ctx *gin.Context) {
